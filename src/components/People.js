88af54a8e92a2577bc12b81a6ed7b506930973f8
juyeon <juyeonh@rainist.com>
2020-12-27 23:18:12 +0900

    Add person component

--- src/components/People.js
+++ src/components/People.js
@@ -1,9 +1,30 @@
 import React from 'react';
+import { useQuery } from 'react-query';
+import Person from './Person';
+
+const fetchPeople = async () => {
+  const res = await fetch('https://swapi.dev/api/people/');
+
+  return res.json();
+};
 
 function People() {
+  const { data, status } = useQuery('people', fetchPeople);
+
   return (
     <div className="People">
       <h2>People</h2>
+      { status === 'error' && (
+        <div>Error fetching data</div>
+      ) }
+      { status === 'loading' && (
+        <div>Loading data...</div>
+      ) }
+      { status === 'success' && (
+        <div>
+          { data.results.map(person => <Person key={ person.name } person={ person } />) }
+        </div>
+      ) }
     </div>
   );
 }
