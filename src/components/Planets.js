07fa72b4176ef97766679837ebec3875bc0dc808
juyeon <juyeonh@rainist.com>
2020-12-28 00:43:59 +0900

    use infinite query hook for pagination

--- src/components/Planets.js
+++ src/components/Planets.js
@@ -1,23 +1,70 @@
-import React from 'react';
-import { useQuery } from 'react-query';
+import React, { useState } from 'react';
+import { useInfiniteQuery } from 'react-query';
 import Planet from './Planet';
 
-const fetchPlanets = async () => {
-  const res = await fetch('https://swapi.dev/api/planets/');
+const fetchPlanets = async ({ pageParam = 1 }) => {
+  const res = await fetch(`https://swapi.dev/api/planets/?page=${pageParam}`);
 
   return res.json();
 };
 
 function Planets() {
-  const { data, status } = useQuery('planets', fetchPlanets, {
-    staleTime: 1000,
+  const [currentPage, setCurrentPage] = useState(0);
+  const [currentData, setCurrentData] = useState(null);
+  const {
+    data,
+    status,
+    hasNextPage,
+    hasPreviousPage,
+    fetchNextPage,
+    fetchPreviousPage,
+  } = useInfiniteQuery('planets', fetchPlanets, {
+    staleTime: 1000000,
     // cacheTime: 10,
-    onSuccess: () => console.log('data fetched with no problem')
+    getNextPageParam: (lastPage) => {
+      const { next } = lastPage;
+
+      return next && next.slice(next.lastIndexOf('=') + 1);
+    },
+    getPreviousPageParam: (firstPage) => {
+      const { previous } = firstPage;
+
+      return previous && previous.slice(previous.lastIndexOf('=') + 1);
+    },
+    onSuccess: ({ pages }) => {
+      setCurrentData(pages[currentPage].results);
+    },
+    // getFetchMore: (lastGroup, allGroup) => {
+    // }
   });
+  console.log('🚀 ~ file: Planets.js ~ line 22 ~ Planets ~ status', status);
+  console.log('data', data);
 
   return (
     <div className="Planets">
       <h2>Planets</h2>
+      <button
+        disabled={ !hasNextPage }
+        onClick={ () => {
+          if (hasNextPage) {
+            fetchNextPage();
+            setCurrentPage(currentPage + 1);
+          }
+        } }
+      >
+        Next
+      </button>
+      <button
+        disabled={ !hasPreviousPage }
+        onClick={ () => {
+          if (hasPreviousPage) {
+            fetchPreviousPage();
+            setCurrentPage(currentPage - 1);
+          }
+        } }
+      >
+        Prev
+      </button>
       { status === 'error' && (
         <div>Error fetching data</div>
       ) }
@@ -26,7 +73,7 @@ function Planets() {
       ) }
       { status === 'success' && (
         <div>
-          { data.results.map(planet => <Planet key={ planet.name } planet={ planet } />) }
+          { currentData.map(planet => <Planet key={ planet.name } planet={ planet } />) }
         </div>
       ) }
     </div>
